2c2a6c69665454e4ff016052e6f3caec7817dc44
Xingzhi Zheng <hi@zhengxingzhi.com>
2023-08-02 14:01:52 +0100

    First Commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import Phaser from 'phaser';
+import Vision from '@mediapipe/tasks-vision';
+
+// const gameConfig: Phaser.Types.Core.GameConfig = {
+//   title: 'Sample',
+ 
+//   type: Phaser.AUTO,
+ 
+//   scale: {
+//     width: window.innerWidth,
+//     height: window.innerHeight,
+//   },
+ 
+//   physics: {
+//     default: 'arcade',
+//     arcade: {
+//       debug: true,
+//     },
+//   },
+ 
+//   parent: 'game',
+//   backgroundColor: '#FFF',
+// };
+ 
+// export const game = new Phaser.Game(gameConfig);
