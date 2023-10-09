43b354fcf77911c269aba956d3e5f9fcbbc62f33
wujiashun <jiashunwu2001@163.com>
2023-10-10 00:43:28 +0800

    update chapter1

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,45 @@
+import { Detector } from "./faceLandmarkDetector";
+import * as Phaser from "phaser";
 
-import { Detector } from './faceLandmarkDetector'
-import * as Phaser from 'phaser'
+import { MainMenuScene } from "./scenes/mainMenuScene";
+import { Chapter1Scene } from "./scenes/chapter1Scene";
+import { Chapter2Scene } from "./scenes/chapter2Scene";
+import { Chapter3Scene } from "./scenes/chapter3Scene";
+import { Chapter4Scene } from "./scenes/chapter4Scene";
+import { Chapter5Scene } from "./scenes/chapter5Scene";
+import { FaceDetectorScene } from "./FaceDetectorScene";
 
-import { MainMenuScene } from './scenes/mainMenuScene'
-import { Chapter1Scene } from './scenes/chapter1Scene'
-import { Chapter2Scene } from './scenes/chapter2Scene'
-import { Chapter3Scene } from './scenes/chapter3Scene'
-import { Chapter4Scene } from './scenes/chapter4Scene'
-import { Chapter5Scene } from './scenes/chapter5Scene'
-import { FaceDetectorScene } from './FaceDetectorScene'
-
-const gameConfig: Phaser.Types.Core.GameConfig = 
-{
-  title: 'focus-but-what',
+const gameConfig: Phaser.Types.Core.GameConfig = {
+  title: "focus-but-what",
   type: Phaser.AUTO,
   scale: {
     width: window.innerWidth,
     height: window.innerHeight,
   },
-  physics: 
-  {
-    default: 'arcade',
+  physics: {
+    default: "arcade",
     arcade: { debug: true },
   },
-  parent: 'game',
-  backgroundColor: '#000000',
-  scene: [ MainMenuScene, Chapter1Scene, Chapter2Scene, Chapter3Scene, Chapter4Scene, Chapter5Scene]
+  parent: "game",
+  backgroundColor: "#000000",
+  scene: [
+    Chapter1Scene,
+    Chapter2Scene,
+    Chapter3Scene,
+    Chapter4Scene,
+    Chapter5Scene,
+  ],
 };
 
 export const game = new Phaser.Game(gameConfig);
 
-async function app() 
-{
-	try 
-  {   
-    await Detector.setup()
-    FaceDetectorScene.currentScene = game.scene.getAt(0)
-  } 
-  catch (error) { console.log(error) }
-};
+async function app() {
+  try {
+    await Detector.setup();
+    FaceDetectorScene.currentScene = game.scene.getAt(0);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 app();
--- src/scenes/chapter1Scene.ts
+++ src/scenes/chapter1Scene.ts
@@ -1,4 +1,5 @@
 import { BlinkingStatus, FaceDetectorScene } from "../FaceDetectorScene";
+import { Detector } from "../faceLandmarkDetector";
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   active: false,
@@ -7,40 +8,106 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 };
 
 export class Chapter1Scene extends FaceDetectorScene {
+  private eyeMask!: Phaser.GameObjects.Image;
+  private maskX = Phaser.Math.Between(0, 800);
+  private maskY = Phaser.Math.Between(0, 600);
+
+  private phonesPosition = [
+    { x: 500, y: 500 },
+    { x: 600, y: 600 },
+  ];
+
+  private scope = 100;
+
+  private phoneMap = new Map();
+
+  isNear = (x1: number, y1: number, x2: number, y2: number) => {
+    return Math.abs(x1 - x2) < this.scope && Math.abs(y1 - y2) < this.scope;
+  };
+
   constructor() {
     super(sceneConfig);
   }
 
   public preload() {
     console.log("Preload");
+    this.load.image("eyeMask", "/EyeMask.svg");
     this.load.multiatlas(
       "Chapter1",
       "/Chapter1/chapter_01_50__300ppi.json",
       "/Chapter1/"
     );
+    console.log(this.phonesPosition);
   }
 
   public create() {
-    const atlasTexture = this.textures.get("Chapter1");
-    if (atlasTexture) {
-      const frameNames = atlasTexture.getFrameNames();
-      console.log(frameNames);
+    this.eyeMask = this.add.image(this.maskX, this.maskY, "eyeMask");
+    const mask = this.eyeMask.createBitmapMask(undefined);
+
+    const backgrounds = [];
+    for (let i = 0; i < 3; i++) {
+      backgrounds.push(
+        this.add.sprite(
+          window.innerWidth / 2,
+          window.innerHeight / 2,
+          "Chapter1",
+          `300ppi/background_0${i + 1}.png`
+        )
+      );
+      backgrounds[i].setScale(
+        window.innerWidth / backgrounds[i].width,
+        window.innerHeight / backgrounds[i].height
+      );
+      backgrounds[i].setMask(mask);
     }
-    const sprite = this.add.sprite(
-      window.innerWidth / 2,
-      window.innerHeight / 2,
-      "Chapter1",
-      "300ppi/background_03.png"
-    );
-    sprite.setScale(
-      window.innerWidth / sprite.width,
-      window.innerHeight / sprite.height
-    );
   }
 
   public update() {
     // TODO
+
+    this.eyeMask.setX(Detector.default!.translateX * window.innerWidth);
+    this.eyeMask.setY(Detector.default!.translateY * window.innerHeight);
+    // console.log(
+    //   Detector.default!.translateX * window.innerWidth,
+    //   Detector.default!.translateY * window.innerHeight
+    // );
   }
 
-  onBlinkStatusChanged(status: BlinkingStatus): void {}
+  onBlinkStatusChanged(status: BlinkingStatus): void {
+    switch (status) {
+      case BlinkingStatus.None:
+        console.log("None");
+        return;
+      case BlinkingStatus.LeftEye:
+        console.log("LeftEye");
+        return;
+      case BlinkingStatus.RightEye:
+        console.log(
+          "RightEye",
+          Detector.default!.translateX * window.innerWidth,
+          Detector.default!.translateY * window.innerHeight
+        );
+        for (let i = 0; i < this.phonesPosition.length; i++) {
+          if (
+            this.isNear(
+              this.phonesPosition[i].x,
+              this.phonesPosition[i].y,
+              Detector.default!.translateX * window.innerWidth,
+              Detector.default!.translateY * window.innerHeight
+            )
+          ) {
+            this.phoneMap.set(i, true);
+            console.log(this.phoneMap);
+          }
+        }
+        if (this.phoneMap.size === this.phonesPosition.length) {
+          this.scene.start("Chapter2");
+        }
+
+        return;
+      case BlinkingStatus.Both:
+        console.log("Both");
+        return;
+    }
+  }
 }
