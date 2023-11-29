7d6ced5500d11ca11e61828b5813432ddcd7a08b
Jiashun Wu <jiashunwu2001@163.com>
2023-11-29 22:57:40 +0800

    update: fix chapter1

--- src/Scenes/Chapter1Scene.ts
+++ src/Scenes/Chapter1Scene.ts
@@ -20,31 +20,21 @@ export class Chapter1Scene extends FaceDetectorScene {
   // private eyeMask!: Phaser.GameObjects.Image;
   private blackBackground!: Phaser.GameObjects.Rectangle;
 
-  private backgrounds = [] as Phaser.GameObjects.Sprite[];
-  private phones = [] as Phaser.GameObjects.Sprite[];
+  private backgroundSprites!: Phaser.GameObjects.Group;
+  private phoneSprites!: Phaser.GameObjects.Group;
 
-  // private maskX = Phaser.Math.Between(0, 800);
-  // private maskY = Phaser.Math.Between(0, 600);
-
-  // private windowWidth = window.innerWidth;
-  // private windowHeight = window.innerHeight;
-
-  // private backgroundImageWidth = 8525;
-  // private backgroundImageHeight = 4796;
-
-  // private widthScale = this.windowWidth / this.backgroundImageWidth;
-  // private heightScale = this.windowHeight / this.backgroundImageHeight;
+  private scaleRate!: number;
 
   private phonesPosition = [
-    { x: 833.56, y: 1913.68 },
-    { x: 2475.68, y: 1279.96 },
-    { x: -629.34, y: 838.02 },
-    { x: 350.1, y: 612.88 },
-    { x: -475.138, y: 120.91 },
-    { x: 516.81, y: 112.57 },
-    { x: -1271.18, y: -341.88 },
-    { x: 33.34, y: -358.55 },
-    { x: -129.2, y: -1438.39 },
+    { x: 833.56, y: -1913.68 },
+    { x: 2475.68, y: -1279.96 },
+    { x: -629.34, y: -838.02 },
+    { x: 350.1, y: -612.88 },
+    { x: -475.138, y: -120.91 },
+    { x: 516.81, y: -112.57 },
+    { x: -1271.18, y: 341.88 },
+    { x: 33.34, y: 358.55 },
+    { x: -129.2, y: 1438.39 },
   ];
 
   private phoneTrigger = [1, 5, 9];
@@ -67,25 +57,12 @@ export class Chapter1Scene extends FaceDetectorScene {
         key: object.texture.key,
         frame: frameName,
       })),
-      frameRate: 0.5,
+      frameRate: 0.2,
       repeat: -1,
     });
+    console.log(anim);
     object.anims.play("frameAnimation");
   };
-
-  // private iconList = ["flag.png", "oil.png", "flowers.png"];
-  // private icons!: Phaser.GameObjects.Sprite[];
-  // iconTween = (object: Phaser.GameObjects.Sprite) => {
-  //   this.tweens.add({
-  //     targets: object,
-  //     duration: 2000,
-  //     scaleX: 1.5 * this.widthScale,
-  //     scaleY: 1.5 * this.heightScale,
-  //     alpha: 0.7,
-  //     yoyo: true,
-  //     repeat: 0,
-  //   });
-  // };
 
   constructor() {
     super(sceneConfig);
@@ -93,20 +70,6 @@ export class Chapter1Scene extends FaceDetectorScene {
 
   public preload() {
     console.log("Preload");
-
-    //format phone position
-    this.phonesPosition = this.phonesPosition.map((phonePosition) => ({
-      x:
-        (this.sceneWidth / 2 + phonePosition.x) *
-        (this.windowWidth / this.sceneWidth),
-      y:
-        (this.sceneHeight / 2 - phonePosition.y) *
-        (this.windowHeight / this.sceneHeight),
-    }));
-    console.log(this.phonesPosition);
-
-    //load eye mask
-    // this.load.image("eyeMask", "/EyeMask.svg");
 
     //load background
     this.load.multiatlas(
@@ -123,47 +86,64 @@ export class Chapter1Scene extends FaceDetectorScene {
         "/Chapter1/"
       );
     }
-
-    // //load icons
-    // this.load.multiatlas("icons", "/Chapter1/icons.json", "/Chapter1/");
   }
 
   public create() {
+    this.backgroundSprites = this.add.group();
+    this.phoneSprites = this.add.group();
+
     //Load background
     const backgroundsTexture = this.textures.get("backgrounds");
-    for (let i = 0; i < backgroundsTexture.getFrameNames().length; i++) {
-      this.backgrounds.push(
-        this.add.sprite(
-          this.windowWidth / 2,
-          this.windowHeight / 2,
-          "backgrounds",
-          `background_0${i + 1}.png`
-        )
-      );
 
-      this.backgrounds[i].setScale(this.widthScale, this.heightScale);
-      this.backgrounds[i].setDepth(this.depth);
-      this.depth++;
+    for (let i = 0; i < backgroundsTexture.getFrameNames().length; i++) {
+      this.backgroundSprites.create(
+        this.windowWidth / 2,
+        this.windowHeight / 2,
+        "backgrounds",
+        backgroundsTexture.getFrameNames()[i]
+      );
     }
 
-    //Load Phones
+    this.backgroundSprites.children.iterate(
+      (sprite: Phaser.GameObjects.GameObject) => {
+        if (sprite instanceof Phaser.GameObjects.Sprite) {
+          sprite.setScale(this.widthScale, this.heightScale);
+          sprite.setDepth(this.depth);
+          this.depth++;
+        }
+        return true;
+      }
+    );
+
+    //format phone position
+    this.phonesPosition = this.phonesPosition.map((phonePosition) => ({
+      x: (this.sceneWidth / 2 + phonePosition.x) * this.widthScale,
+      y: (this.sceneHeight / 2 + phonePosition.y) * this.heightScale,
+    }));
+    console.log(this.phonesPosition);
 
     for (let i = 0; i < this.phonesPosition.length; i++) {
       const texture = this.textures.get(`phone_0${i + 1}`);
-      this.phones.push(
-        this.add.sprite(
-          this.phonesPosition[i].x,
-          this.phonesPosition[i].y,
-          `phone_0${i + 1}`,
-          texture.getFrameNames()[0]
-        )
+      this.phoneSprites.create(
+        this.phonesPosition[i].x,
+        this.phonesPosition[i].y,
+        `phone_0${i + 1}`,
+        texture.getFrameNames()[0]
       );
-
-      this.phones[i].setScale(this.widthScale, this.heightScale);
-      this.playFrameAnimation(this.phones[i], texture.getFrameNames());
-      this.phones[i].setDepth(this.depth);
-      this.depth++;
     }
+
+    //Load Phones
+    this.phoneSprites.children.iterate(
+      (sprite: Phaser.GameObjects.GameObject) => {
+        if (sprite instanceof Phaser.GameObjects.Sprite) {
+          sprite.setScale(this.widthScale, this.heightScale);
+          this.playFrameAnimation(sprite, sprite.texture.getFrameNames());
+          sprite.setDepth(this.depth);
+          this.depth++;
+        }
+        return true;
+      }
+    );
 
     //Load Eye Mask
     this.blackBackground = this.add.rectangle(
@@ -176,51 +156,57 @@ export class Chapter1Scene extends FaceDetectorScene {
     this.mask.invertAlpha = true;
     this.blackBackground.setDepth(100);
     this.blackBackground.setMask(this.mask);
+  }
 
-    // const eyeMask = this.add.image(
-    //   window.innerWidth / 2,
-    //   window.innerHeight / 2,
-    //   "interface", "eye_view_01"
-    // );
-    // eyeMask.setDepth(100);
-    // eyeMask.setScale(0.1);
+  updatePostionAndScale(translateX: number, translateY: number, scale: number) {
+    translateX = translateX - 0.3;
+    translateY = translateY - 0.3;
 
-    // this.eyeMask = this.add.image(
-    //   this.windowWidth / 2,
-    //   this.windowHeight / 2,
-    //   "eyeMask"
-    // );
-    // console.log(this.eyeMask.width, this.eyeMask.height);
-    // console.log(this.windowWidth, this.windowHeight);
-    // this.eyeMask.setScale(
-    //   ((this.windowWidth / this.eyeMask.width) * 2) / 3,
-    //   ((this.windowHeight / this.eyeMask.height) * 2) / 3
-    // );
-    // const mask = this.eyeMask.createBitmapMask();
+    this.scaleRate = 2 / scale;
 
-    // mask.invertAlpha = true;
-    // this.blackBackground.setMask(mask);
+    this.backgroundSprites.children.iterate(
+      (sprite: Phaser.GameObjects.GameObject) => {
+        if (sprite instanceof Phaser.GameObjects.Sprite) {
+          sprite.setPosition(
+            translateX * window.innerWidth * this.scaleRate,
+            translateY * window.innerHeight * this.scaleRate
+          );
 
-    //Load Icons
-    // this.icons = [];
-    // for (let i = 0; i < this.iconList.length; i++) {
-    //   this.icons.push(
-    //     this.add.sprite(
-    //       this.windowWidth / 2 + (this.windowWidth / 6) * (i - 1),
-    //       (this.windowHeight / 10) * 9,
-    //       "icons",
-    //       this.iconList[i]
-    //     )
-    //   );
-    //   this.icons[i].setDepth(1000);
-    //   this.icons[i].setScale(this.widthScale, this.heightScale);
-    // }
+          sprite.setScale(
+            this.widthScale * this.scaleRate,
+            this.heightScale * this.scaleRate
+          );
+        }
+        return true;
+      }
+    );
+
+    this.phoneSprites.children.iterate(
+      (sprite: Phaser.GameObjects.GameObject, index) => {
+        let nowX =
+          (translateX * window.innerWidth +
+            this.phonesPosition[index].x -
+            this.windowWidth / 2) *
+          this.scaleRate;
+        let nowY =
+          (translateY * window.innerHeight +
+            this.phonesPosition[index].y -
+            this.windowHeight / 2) *
+          this.scaleRate;
+        if (sprite instanceof Phaser.GameObjects.Sprite) {
+          sprite.setPosition(nowX, nowY);
+          sprite.setScale(
+            this.widthScale * this.scaleRate,
+            this.heightScale * this.scaleRate
+          );
+        }
+        return true;
+      }
+    );
   }
 
   public update() {
     // TODO
-    // this.eyeMask.setX(Detector.default!.translateX * window.innerWidth);
-    // this.eyeMask.setY(Detector.default!.translateY * window.innerHeight);
 
     this.blackBackground.width = this.windowWidth;
     this.blackBackground.height = this.windowHeight;
@@ -234,22 +220,11 @@ export class Chapter1Scene extends FaceDetectorScene {
       Detector.default!.translateY >= heightScope &&
       Detector.default!.translateY <= 1 - heightScope
     ) {
-      this.backgrounds.forEach((background) => {
-        background.setX(Detector.default!.translateX * window.innerWidth);
-        background.setY(Detector.default!.translateY * window.innerHeight);
-      });
-      this.phones.forEach((phone, index) => {
-        phone.setX(
-          Detector.default!.translateX * window.innerWidth +
-            this.phonesPosition[index].x -
-            this.windowWidth / 2
-        );
-        phone.setY(
-          Detector.default!.translateY * window.innerHeight +
-            this.phonesPosition[index].y -
-            this.windowHeight / 2
-        );
-      });
+      this.updatePostionAndScale(
+        Detector.default!.translateX,
+        Detector.default!.translateY,
+        Detector.default!.scale
+      );
     }
   }
 
